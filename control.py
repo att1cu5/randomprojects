da83d2a919818edb1f328b917266cea9e80298ed
att1cu5 <atticusmgr27@rdcrs.ca>
2026-03-10 13:02:03 -0600

    Refactor calculations and variable assignments in control.py

--- control.py
+++ control.py
@@ -1,15 +1,18 @@
 uio=[3,4,56,7,34,9,20,23]
 k=[complex(8,3),complex(4,6),complex(3,6),complex(3,5),complex(0,1)]
 deltau=1
-Q=1
+Q=0
 OP=5
 LO=[0,0,0,0,0,0,0,0,0,0,0]
-R=2
+OL=[0,0,0,0,0,0,0,0,0,0,0]
+R=0
 r=0
 iop=0
+fgo=0
 for i in range(0,5):
-    iop+=(uio[i]*k[i]-uio[i-1]*k[i])**2
+    iop+=((uio[i]*k[i])-(uio[i-1]*k[i]))**2
     LO[i]=iop
-fgo=abs(deltau*k)**2
-print(Q*(iop)+R*(fgo))
-J=Q*(iop)+R*(fgo)
+    fgo+=(deltau*k[i])**2
+    OL[i]=fgo
+print((iop)+(fgo))
+J=(iop)+(fgo)
