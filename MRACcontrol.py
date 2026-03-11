536dd8c2529895e0d6b56e4c4ddca42ab21a9a7d
att1cu5 <atticusmgr27@rdcrs.ca>
2026-03-11 14:53:09 -0600

    Refactor MRAC control loop and coefficient averaging
    Refactor MRAC control logic and improve coefficient calculations.

--- MRACcontrol.py
+++ MRACcontrol.py
@@ -4,52 +4,132 @@ Aavg=0
 Bavg=0
 Cavg=0
 Davg=0
-while(time<1):
-    AMatrix=[[4,10,0],[1,9,0],[0,4,10]]
-    BMatrix=[[0,0,0],[0,0,0],[0,0,0]]
-    np.NMatrix=[[0,0,0],[0,0,0],[0,0,0]]
-    def Xm(t):
-        return t+1
-    def r(t):
-        return (t*t)+t
-    for i in range(0,2):
-        BMatrix[i][0]=(Xm(time)-(AMatrix[i][0]*Xm(time)))/r(time)
-    for i in range(0,2):
-        BMatrix[i][1]=(Xm(time)-(AMatrix[i][1]*Xm(time)))/r(time)    
-    for i in range(0,2):
-        BMatrix[i][2]=(Xm(time)-(AMatrix[i][2]*Xm(time)))/r(time)    
-    for i in range(0,2):
-        np.NMatrix[i][0]=AMatrix[i][0]*Xm(time)+BMatrix[i][0]*r(time)
-    for i in range(0,2):
-        np.NMatrix[i][1]=AMatrix[i][1]*Xm(time)+BMatrix[i][1]*r(time)
-    for i in range(0,2):
-        np.NMatrix[i][2]=AMatrix[i][2]*Xm(time)+BMatrix[i][2]*r(time)
-    
-    
-    
-    # Define a 3x3 matrix
-    A = np.NMatrix
-    
-    # Calculate eigenvalues
-    eigenvalues = np.linalg.eigvals(A)
-    
-    # Convert eigenvalues to polynomial coefficients (p(x) = x^3 + c2*x^2 + c1*x + c0)
-    coeffs = np.poly(eigenvalues)
-    
-    #print("Polynomial Coefficients:", coeffs)
-    coeffsA=coeffs[0]
-    coeffsB=coeffs[1]
-    coeffsC=coeffs[2]
-    coeffsD=coeffs[3]
-    #print(str(coeffsD)+"x^3+"+str(coeffsC)+"+x^2"+str(coeffsB)+"x+"+str(coeffsC))
-    # To evaluate the polynomial function at a value (e.g., x=2):
-    #print("P(",time,") =", np.polyval(coeffs, time))
-    time=time+1
-    Aavg=Aavg+coeffsA
-    Bavg=Bavg+coeffsB
-    Cavg=Cavg+coeffsC
-    Davg=Davg+coeffsD
-print("average A: ",Aavg/time)
-print("average B: ",Bavg/time)
-print("average C: ",Cavg/time)
-print("average D: ",Aavg/time)
+errorA=0
+errorB=0
+errorC=0
+
+while(time<=1):
+            AMatrix=[[4,10,0],[1,9,0],[0,4,10]]
+            BMatrix=[[0,0,0],[0,0,0],[0,0,0]]
+            np.NMatrix=[[0,0,0],[0,0,0],[0,0,0]]
+            def Xm(t):
+                return t+1
+            def r(t):
+                return (t*t)+t
+            for i in range(0,2):
+                BMatrix[i][0]=(Xm(time)-(AMatrix[i][0]*Xm(time)))/r(time)
+            for i in range(0,2):
+                BMatrix[i][1]=(Xm(time)-(AMatrix[i][1]*Xm(time)))/r(time)    
+            for i in range(0,2):
+                BMatrix[i][2]=(Xm(time)-(AMatrix[i][2]*Xm(time)))/r(time)    
+            for i in range(0,2):
+                np.NMatrix[i][0]=AMatrix[i][0]*Xm(time)+BMatrix[i][0]*r(time)
+            for i in range(0,2):
+                np.NMatrix[i][1]=AMatrix[i][1]*Xm(time)+BMatrix[i][1]*r(time)
+            for i in range(0,2):
+                np.NMatrix[i][2]=AMatrix[i][2]*Xm(time)+BMatrix[i][2]*r(time)
+            
+            
+            
+            # Define a 3x3 matrix
+            A = np.NMatrix
+            
+            # Calculate eigenvalues
+            eigenvalues = np.linalg.eigvals(A)
+            
+            # Convert eigenvalues to polynomial coefficients (p(x) = x^3 + c2*x^2 + c1*x + c0)
+            coeffs = np.poly(eigenvalues)
+            
+            #print("Polynomial Coefficients:", coeffs)
+            coeffsA=coeffs[0]
+            coeffsB=coeffs[1]
+            coeffsC=coeffs[2]
+            coeffsD=coeffs[3]
+            print(str(coeffsD)+"x^3+"+str(coeffsC)+"x^2"+str(coeffsB)+"x+"+str(coeffsA))
+            # To evaluate the polynomial function at a value (e.g., x=2):
+            #print("P(",time,") =", np.polyval(coeffs, time))
+            time=time+0.1
+            Aavg=Aavg+coeffsA
+            Bavg=Bavg+coeffsB
+            Cavg=Cavg+coeffsC
+            Davg=Davg+coeffsD
+print("average A: ",Aavg/((time-0.1)/0.1))
+print("average B: ",Bavg/((time-0.1)/0.1))
+print("average C: ",Cavg/((time-0.1)/0.1))
+print("average D: ",Davg/((time-0.1)/0.1))
+Aavg=Aavg/((time-0.1)/0.1)
+Bavg=Bavg/((time-0.1)/0.1)
+Cavg=Cavg/((time-0.1)/0.1)
+Davg=Davg/((time-0.1)/0.1)
+realA=coeffsA
+realB=coeffsB
+realC=coeffsC
+realD=coeffsD
+errorA=abs(Aavg-realA)/realA
+errorB=abs(Bavg-realB)/realB
+errorC=abs(Cavg-realC)/realC
+print("errorA percentage: ",errorA*100,"%")
+print("errorB percentage: ",errorB*100,"%")
+print("errorC percentage: ",errorC*100,"%")
+time=0.1
+
+
+while(time<=1):
+            AMatrix=[[4,10,0],[1,9,0],[0,4,10]]
+            BMatrix=[[0,0,0],[0,0,0],[0,0,0]]
+            np.NMatrix=[[0,0,0],[0,0,0],[0,0,0]]
+            def Xm(t):
+                return t+1
+            def r(t):
+                return (t*t)+t
+            for i in range(0,2):
+                BMatrix[i][0]=(Xm(time)-(AMatrix[i][0]*Xm(time)))/r(time)
+            for i in range(0,2):
+                BMatrix[i][1]=(Xm(time)-(AMatrix[i][1]*Xm(time)))/r(time)    
+            for i in range(0,2):
+                BMatrix[i][2]=(Xm(time)-(AMatrix[i][2]*Xm(time)))/r(time)    
+            for i in range(0,2):
+                np.NMatrix[i][0]=AMatrix[i][0]*Xm(time)+BMatrix[i][0]*r(time)
+            for i in range(0,2):
+                np.NMatrix[i][1]=AMatrix[i][1]*Xm(time)+BMatrix[i][1]*r(time)
+            for i in range(0,2):
+                np.NMatrix[i][2]=AMatrix[i][2]*Xm(time)+BMatrix[i][2]*r(time)
+            
+            
+            
+            # Define a 3x3 matrix
+            A = np.NMatrix
+            
+            # Calculate eigenvalues
+            eigenvalues = np.linalg.eigvals(A)
+            
+            # Convert eigenvalues to polynomial coefficients (p(x) = x^3 + c2*x^2 + c1*x + c0)
+            coeffs = np.poly(eigenvalues)
+            
+            #print("Polynomial Coefficients:", coeffs)
+            coeffsA=coeffs[0]
+            coeffsB=coeffs[1]
+            coeffsC=coeffs[2]
+            coeffsD=coeffs[3]
+            #print(str(coeffsD)+"x^3+"+str(coeffsC)+"x^2"+str(coeffsB)+"x+"+str(coeffsA))
+            # To evaluate the polynomial function at a value (e.g., x=2):
+            #print("P(",time,") =", np.polyval(coeffs, time))
+            time=time+0.1
+            Aavg=Aavg+coeffsA
+            Bavg=Bavg+coeffsB
+            Cavg=Cavg+coeffsC
+            Davg=Davg+coeffsD
+Aavg=Aavg/((time-0.1)/0.1)
+Bavg=Bavg/((time-0.1)/0.1)
+Cavg=Cavg/((time-0.1)/0.1)
+Davg=Davg/((time-0.1)/0.1)
+print("average A: ",Aavg/((time-0.1)/0.1))
+print("average B: ",Bavg/((time-0.1)/0.1))
+print("average C: ",Cavg/((time-0.1)/0.1))
+print("average D: ",Davg/((time-0.1)/0.1))
+print("predicted real value of coefficient C:",realC-(Cavg*errorC))
+print("real value: ",coeffsC)
+print("predicted real value of coefficient A:",realA+(Aavg*errorA))
+print("real value: ",coeffsA)
+print("predicted real value of coefficient A:",realB-(Bavg*errorB))
+print("real value: ",coeffsB)
