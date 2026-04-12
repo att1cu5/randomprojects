1b36b6c6ed2f6740ba1430d92b569b30f32c4ce0
att1cu5 <atticusmgr27@rdcrs.ca>
2026-04-11 21:38:35 -0600

    Clean up MYai.py by removing redundant code
    Removed unused calculations and print statements related to AvgA and AnsA.

--- MYai.py
+++ MYai.py
@@ -328,24 +328,8 @@ for i in range(0,len(endF[0])):
     SUMA+=complex(endF[0,i].real,endF[0,i].imag)
 
 end = time.perf_counter()
-time.sleep(random.random()+0.1)
+
 t=end - start
 hiuo=(6.2607015*math.pow(10,-34))/(2*math.pi)
 Uans=SUMA*math.e**(complex(0,-1)*(t/hiuo))*endF
-AvgA=0
-for i in range(0,len(Uans[0])):
-    AvgA+=complex(Uans[-1,i].real,Uans[-1,i].imag)
-for ik in range(0,len(Uans[0])):
-    AvgA+=complex(Uans[0,i].real,Uans[0,i].imag)
-#print(Uans)
-
-#print(UansA)
-#print(Uans)
-
-AnsA=int(abs(AvgA.imag/AvgA.real)*math.pow(10,15))*int(abs(AvgA.real/AvgA.imag)*math.pow(10,15))
-
-print(AnsA-999999999999900000000000000000)
-
-
-
-
+print(Uans)
