9928645b1aea3b86655d194229f097767d1704bf
att1cu5 <atticusmgr27@rdcrs.ca>
2026-05-08 18:24:09 -0600

    Update print statement to include additional arguments

--- MYai.py
+++ MYai.py
@@ -723,7 +723,7 @@ oper=get_tiny_square_string(ople,ilehj,2)
 
 ilkope=get_tiny_square_string(lopej,oper,2)
 finalsumofprob=get_tiny_square_string(ilkope,ilko,2)
-print(finalsumofprob)
+print(get_tiny_square_string(finalsumofprob,2,5))
 
 
 
