f9b05cad7d6fbd90c0b3dd51e8e38eb9adc3ffa9
att1cu5 <atticusmgr27@rdcrs.ca>
2026-05-31 18:51:49 -0600

    Update rewardsystem.py

--- rewardsystem.py
+++ rewardsystem.py
@@ -1,10 +1,8 @@
-
-
 a=1
 fg=0
-def ran(v,kl):
-    import random
-    return random.uniform(v,kl)
+def ran(v,kl,lo,h):
+    
+    return (kl**2)-(h+lo)%v
 def f(x):
     return x+1
 def g(a):
@@ -46,29 +44,36 @@ def weight(xd,fg,fgo):
 
             fgo+=(klo[0]+klo[1])
     return fg,fgo
-def matrix(x,y):
+def matrix(x,y,kl,lp):
     import numpy as np
     #import random
+    h=0
+    mat=np.array([[0,0],[0,0]])
     #h=random.choice([-1,1])
-    
+    for i in range(kl,lp):
+        if(i%2==0):
+            h=1
+        else:
+            h=-1
     if(h==1):
-        mat=np.array([[x,-y],[-y,x]])
+            mat=np.array([[x,-y],[-y,x]])
     if(h==-1):
-        mat=np.array([[-x,y],[y,-x]])
+            mat=np.array([[-x,y],[y,-x]])
     return mat
-def nmartrix(l,o,d,k,lo,kl):
+def nmartrix(l,o,d,k,lo,kl,op,dpi):
     
-    return matrix(l,o)*weight(d,k,lo)[kl]
+    return matrix(l,o,op,dpi)*weight(d,k,lo)[kl]
 
-def j(v,vf,k,l,p,o,pl): 
-    ko=ran(-1,1)
-    kop=ran(-1,1)
+def j(v,vf,k,l,p,o,pl,ok,plk,plkl,okl): 
+    ko=ran(3,2,-1,4)
+    kop=ran(5,6,-1,8)
     for i in range(v,vf):
-        ko+=nmartrix(k,l,p,o,pl,0)
-        kop+=nmartrix(k,l,p,o,pl,1)
+        ko+=nmartrix(k,l,p,o,pl,0,plk,ok)
+        kop+=nmartrix(k,l,p,o,pl,1,plkl,okl)
     return ko,kop
 def hu():
     #import random
     #h=random.choice([0,1])
-    return j(-3,3,0.2,0.2,3,4,3)[0]*j(-3,3,0.2,0.2,3,4,3)[1]
+    #j(-4,3,0.6,0.2,3,4,3,-4,6,-2,7)[1]
+    return j(-4,3,2,-0.2,3,4,3,-2,2,3,-3)[0]*j(-4,3,-0.2,2,3,4,3,-2,2,5,-3)[1]
 print(hu())
