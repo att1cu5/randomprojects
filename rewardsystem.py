679088be11064286ec2587a289b042d5ea65e16f
att1cu5 <atticusmgr27@rdcrs.ca>
2026-05-31 18:06:26 -0600

    Refactor rewardsystem.py with new functions and logic
    Refactor weight function to handle additional parameters and improve logic. Introduce new functions for random number generation and matrix operations.

--- rewardsystem.py
+++ rewardsystem.py
@@ -1,8 +1,10 @@
 
-output=0
-inputs=0
+
 a=1
 fg=0
+def ran(v,kl):
+    import random
+    return random.uniform(v,kl)
 def f(x):
     return x+1
 def g(a):
@@ -17,17 +19,56 @@ def g(a):
         except ZeroDivisionError:
             k=math.inf
     return k,output
-
 def ranged(ih):
     hj=g(ih)
     ol=g(-ih)
     jio=ol[0]/ol[1]
     jok=hj[0]/hj[1]
     return jio,jok
-def weight(xd):
-    fg=0
-    for i in range(xd,-1):
-        klo=ranged(i/(10**i))
+def weight(xd,fg,fgo):
+    if(xd<0):
+        for i in range(xd,-1):
+            klo=ranged(i/(10**i))[0]
 
-        fg+=(klo[0]+klo[1])
-    return fg
+            fg+=(klo[0]+klo[1])
+        for i in range(xd,-1):
+            klo=ranged(i/(10**i))[1]
+
+            fgo+=(klo[0]+klo[1])
+    #print(fg)
+    else:
+        for i in range(1,xd):
+            klo=ranged(i/(10**i))
+
+            fg+=(klo[0]+klo[1])
+        for i in range(1,xd):
+            klo=ranged(i/(10**i))
+
+            fgo+=(klo[0]+klo[1])
+    return fg,fgo
+def matrix(x,y):
+    import numpy as np
+    #import random
+    #h=random.choice([-1,1])
+    
+    if(h==1):
+        mat=np.array([[x,-y],[-y,x]])
+    if(h==-1):
+        mat=np.array([[-x,y],[y,-x]])
+    return mat
+def nmartrix(l,o,d,k,lo,kl):
+    
+    return matrix(l,o)*weight(d,k,lo)[kl]
+
+def j(v,vf,k,l,p,o,pl): 
+    ko=ran(-1,1)
+    kop=ran(-1,1)
+    for i in range(v,vf):
+        ko+=nmartrix(k,l,p,o,pl,0)
+        kop+=nmartrix(k,l,p,o,pl,1)
+    return ko,kop
+def hu():
+    #import random
+    #h=random.choice([0,1])
+    return j(-3,3,0.2,0.2,3,4,3)[0]*j(-3,3,0.2,0.2,3,4,3)[1]
+print(hu())
